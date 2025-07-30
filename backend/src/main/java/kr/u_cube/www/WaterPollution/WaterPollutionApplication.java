0fb3f133f0cb6cebad00d7e835b41583e383b6ed
point73 <whdydwns73@naver.com>
2025-07-30 14:51:23 +0900

    feat: backend 프로젝트 생성

---
+++ backend/src/main/java/kr/u_cube/www/WaterPollution/WaterPollutionApplication.java
@@ -0,0 +1,13 @@
+package kr.u_cube.www.WaterPollution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterPollutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterPollutionApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/kr/u_cube/www/WaterPollution/WaterPollutionApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.u_cube.www.WaterPollution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterPollutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
