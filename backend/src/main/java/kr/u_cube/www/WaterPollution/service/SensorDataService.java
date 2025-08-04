16ccbaa1b24ec0a8f78160c29ce6d3fe27464f0b
YongJun Cho <whdydwns73@naver.com>
2025-08-04 23:12:34 +0900

    refactor: SensorDataService 불필요 임포트 정리

--- backend/src/main/java/kr/u_cube/www/WaterPollution/service/SensorDataService.java
+++ backend/src/main/java/kr/u_cube/www/WaterPollution/service/SensorDataService.java
@@ -3,7 +3,6 @@ package kr.u_cube.www.WaterPollution.service;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
